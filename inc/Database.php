db4e57929f852937754ec89431722069e91c07bf
jurentie <jurentie@rams.colostate.edu>
2017-04-06 11:02:58 -0600

    added writeComment() function

---
+++ inc/Database.php
@@ -0,0 +1,44 @@
+<?php
+
+	class Database extends PDO{
+		public function __construct() {
+			//Directory to music database
+			parent::__construct ( "sqlite:../ingredients.db" );
+		}
+
+		//returns an array of ingredient values
+	function getIngredient($name){
+
+		$ingredient = array();
+
+		$ingredient[0] = $name;
+
+		$sql = "SELECT description FROM ingredients WHERE name LIKE '%$name%'";
+
+		$description = $this->query($sql);
+
+		$ingredient[1] = $description->fetchColumn();
+
+		$sql = "SELECT image FROM ingredients WHERE name LIKE '%$name%'";
+
+		$image = $this->query($sql);
+
+		$ingredient[2] = $image->fetchColumn();
+
+		return $ingredient;
+
+	}
+	function writeComment($name, $comment, $ip, $time){
+
+		$sql = "INSERT INTO comments (name, comment, ip, time) VALUES ('" . $name . "', '" . $comment . "', '" . $ip . "', '" . $time . "')";
+
+		$status = $this->exec($sql);
+		if($status === FALSE){
+			echo '<pre class="bg-danger">';
+			print_r ( $this->errorInfo () );
+			echo '</pre>';
+		}
+
+	}
+}
+?>
